0f21639b7032fb69771effe67d0a86159b7327da
Dang Kham <danqkhaw72@gmail.com>
2020-08-09 16:30:02 +0700

    Create AccountDAO(target object), Spring java config class, main app

---
+++ src/com/kamu/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.kamu.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.kamu.aopdemo")
+public class DemoConfig {
+
+}
